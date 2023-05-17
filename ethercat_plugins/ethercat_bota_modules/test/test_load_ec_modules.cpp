476853726c10dd6f8c366aaa6e810cb62f067e36
Dominic Reber <71256590+domire8@users.noreply.github.com>
2023-05-17 10:06:09 +0200

    Rename bota plugin (#4)

--- ethercat_plugins/ethercat_bota_modules/test/test_load_ec_modules.cpp
+++ ethercat_plugins/ethercat_bota_modules/test/test_load_ec_modules.cpp
@@ -4,10 +4,10 @@
 #include <pluginlib/class_loader.hpp>
 #include "ethercat_interface/ec_slave.hpp"
 
-TEST(TestLoadBota_SensONE, load_ec_module)
+TEST(TestLoadBota_FTSensor, load_ec_module)
 {
   pluginlib::ClassLoader<ethercat_interface::EcSlave> ec_loader_{
     "ethercat_interface", "ethercat_interface::EcSlave"};
-  ASSERT_NO_THROW(ec_loader_.createSharedInstance("ethercat_plugins/Bota_SensONE"));
+  ASSERT_NO_THROW(ec_loader_.createSharedInstance("ethercat_plugins/Bota_FTSensor"));
 }
 
